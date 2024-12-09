6aad91ce240c046c209296e02a5e58b564377674
Tiago Santos <tiagosn06@gmail.com>
2024-12-09 19:08:28 -0300

    remove static files

--- setup/settings.py
+++ setup/settings.py
@@ -32,7 +32,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "django.contrib.staticfiles",
+    # "django.contrib.staticfiles",
     "rest_framework",
     "app.apps.AppConfig",
 ]
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+# STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
