78cf8902cd7d3d685a20e2ca4f495dea64eaaec0
Tiago Santos <tiagosn06@gmail.com>
2024-12-09 18:59:17 -0300

    disable debug

--- setup/settings.py
+++ setup/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG")
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
